b952d4e2829bc255808860fba75c84b2213fb8a2
cockmaster69 p <kijo@gmx.net>
2025-04-29 17:06:28 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import VenoxHeader from "./components/header/venoxheader.vue"
+</script>
+
+<style>
+@import "tailwindcss";
+</style>
+
+<template>
+  <header>
+    <VenoxHeader />
+  </header>
+
+  <main>
+  </main>
+
+  <footer>
+  </footer>
+</template>
---
+++ src/components/header/venoxheader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p class="mx-auto bg-red-50">hey there</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
