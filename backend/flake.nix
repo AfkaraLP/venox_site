ee6e2f806139eb320910dc9950958ec5ddc67f69
AfkaraLP <kijo@gmx.net>
2025-07-25 10:52:31 -0500

    more flakey

--- backend/flake.nix
+++ backend/flake.nix
@@ -57,5 +57,21 @@
           };
         };
       });
+
+      packages = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.rustPlatform.buildRustPackage {
+          pname = "venox-backend";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [pkg-config];
+          buildInputs = with pkgs; [ openssl sqlite ];
+        };
+      });
     };
 }
