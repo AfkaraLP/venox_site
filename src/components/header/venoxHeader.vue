0a7b119b0c4fe841632b1ef128099db0a68f8137
AfkaraLP <kijo@gmx.net>
2025-07-25 15:55:02 +0200

    no vue logo

--- src/components/header/venoxHeader.vue
+++ src/components/header/venoxHeader.vue
@@ -3,7 +3,7 @@
     <div class="logo-container-mc">
       <a href="/">
         <span class="sr-only">Venox Home</span>
-        <span class="logo-svg-mc" v-html="logoSvg"></span>
+        <!-- <span class="logo-svg-mc" v-html="logoSvg"></span> -->
       </a>
       <span class="site-title-mc">LILVENOX</span>
     </div>
@@ -17,7 +17,7 @@
 </template>
 
 <script setup lang="ts">
-const logoSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69" width="38" height="38"><path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883"/><path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e"/></svg>`
+// const logoSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69" width="38" height="38"><path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883"/><path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e"/></svg>`
 </script>
 
 <style scoped>
